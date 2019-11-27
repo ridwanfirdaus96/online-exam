6f064716ce5e59036414f0647fd7f8aff0223920
fainsz <irfan_nugraha@ymail.com>
2019-11-27 08:33:47 +0700

    New

--- project1.sql
+++ project1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 01, 2018 at 03:08 PM
--- Server version: 10.1.36-MariaDB
--- PHP Version: 7.2.10
+-- Host: 127.0.0.1
+-- Generation Time: Nov 27, 2019 at 02:29 AM
+-- Server version: 10.4.8-MariaDB
+-- PHP Version: 7.1.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -39,10 +39,13 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`email`, `password`, `role`) VALUES
-('head@gmail.com', 'head', 'head'),
-('teacher1@gmail.com', 'teacher1', 'admin'),
-('teacher2@gmail.com', 'teacher2', 'admin'),
-('teacher3@gmail.com', 'teacher3', 'admin');
+('admin@gmail.com', 'admin', 'head'),
+('agama@gmail.com', 'agama', 'admin'),
+('indonesia@gmail.com', 'indonesia', 'admin'),
+('inggris@gmail.com', 'inggris', 'admin'),
+('matematika@gmail.com', 'matematika', 'admin'),
+('pancasila@gmail.com', 'pancasila', 'admin'),
+('sejarah@gmail.com', 'sejarah', 'admin');
 
 -- --------------------------------------------------------
 
@@ -60,25 +63,29 @@ CREATE TABLE `answer` (
 --
 
 INSERT INTO `answer` (`qid`, `ansid`) VALUES
-('55892169bf6a7', '55892169d2efc'),
-('5589216a3646e', '5589216a48722'),
-('558922117fcef', '5589221195248'),
-('55892211e44d5', '55892211f1fa7'),
-('558922894c453', '558922895ea0a'),
-('558922899ccaa', '55892289aa7cf'),
-('558923538f48d', '558923539a46c'),
-('55892353f05c4', '55892354051be'),
-('558973f4389ac', '558973f462e61'),
-('558973f4c46f2', '558973f4d4abe'),
-('558973f51600d', '558973f526fc5'),
-('558973f55d269', '558973f57af07'),
-('558973f5abb1a', '558973f5e764a'),
-('5589751a63091', '5589751a81bf4'),
-('5589751ad32b8', '5589751adbdbd'),
-('5589751b304ef', '5589751b3b04d'),
-('5589751b749c9', '5589751b9a98c'),
-('5bd1a29b0514c', '5bd1a29b1c417'),
-('5bd1a29b7d4b8', '5bd1a29b8ae6e');
+('5ddd606a6ffbe', '5ddd606a70736'),
+('5ddd606a73dda', '5ddd606a74475'),
+('5ddd63a65fc6f', '5ddd63a6603d3'),
+('5ddd63a662fb0', '5ddd63a663721'),
+('5ddd650773e47', '5ddd6507745ba'),
+('5ddd6507773f7', '5ddd650777a7f'),
+('5ddd66c058f87', '5ddd66c059662'),
+('5ddd66c05b4db', '5ddd66c05c443'),
+('5ddd68c4a9ac3', '5ddd68c4aa291'),
+('5ddd68c4adf85', '5ddd68c4ae7f5'),
+('5ddd6a196b714', '5ddd6a196bf27'),
+('5ddd6a196f4c9', '5ddd6a1970528');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `audios`
+--
+
+CREATE TABLE `audios` (
+  `id` int(11) NOT NULL,
+  `filename` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -109,8 +116,26 @@ CREATE TABLE `history` (
   `level` int(11) NOT NULL,
   `sahi` int(11) NOT NULL,
   `wrong` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`email`, `eid`, `score`, `level`, `sahi`, `wrong`, `date`) VALUES
+('fauziah@gmail.com', '5ddd66f1d08b8', 1, 2, 1, 1, '2019-11-27 01:19:00'),
+('ridwan@gmail.com', '5ddd6545d57ac', 4, 2, 2, 0, '2019-11-27 01:19:43'),
+('asep@gmail.com', '5ddd6320142aa', 4, 2, 2, 0, '2019-11-27 01:20:48'),
+('ragil@gmail.com', '5ddd5e51a7a39', -2, 2, 0, 2, '2019-11-27 01:21:12'),
+('ahmad@gmail.com', '5ddd694bca4d7', 1, 2, 1, 1, '2019-11-27 01:22:20'),
+('monica@gmail.com', '5ddd642381974', 4, 2, 2, 0, '2019-11-27 01:22:45'),
+('septian@gmail.com', '5ddd6545d57ac', -2, 2, 0, 2, '2019-11-27 01:23:07'),
+('valent@gmail.com', '5ddd66f1d08b8', -2, 2, 0, 2, '2019-11-27 01:23:27'),
+('andhini@gmail.com', '5ddd694bca4d7', 4, 2, 2, 0, '2019-11-27 01:24:09'),
+('andhini@gmail.com', '5ddd6320142aa', 4, 2, 2, 0, '2019-11-27 01:24:17'),
+('anisa@gmail.com', '5ddd6320142aa', -2, 2, 0, 2, '2019-11-27 01:25:26'),
+('anisa@gmail.com', '5ddd5e51a7a39', 1, 2, 1, 1, '2019-11-27 01:25:34');
 
 -- --------------------------------------------------------
 
@@ -129,83 +154,54 @@ CREATE TABLE `options` (
 --
 
 INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
-('55892169bf6a7', 'usermod', '55892169d2efc'),
-('55892169bf6a7', 'useradd', '55892169d2f05'),
-('55892169bf6a7', 'useralter', '55892169d2f09'),
-('55892169bf6a7', 'groupmod', '55892169d2f0c'),
-('5589216a3646e', '751', '5589216a48713'),
-('5589216a3646e', '752', '5589216a4871a'),
-('5589216a3646e', '754', '5589216a4871f'),
-('5589216a3646e', '755', '5589216a48722'),
-('558922117fcef', 'echo', '5589221195248'),
-('558922117fcef', 'print', '558922119525a'),
-('558922117fcef', 'printf', '5589221195265'),
-('558922117fcef', 'cout', '5589221195270'),
-('55892211e44d5', 'int a', '55892211f1f97'),
-('55892211e44d5', '$a', '55892211f1fa7'),
-('55892211e44d5', 'long int a', '55892211f1fb4'),
-('55892211e44d5', 'int a$', '55892211f1fbd'),
-('558922894c453', 'cin>>a;', '558922895ea0a'),
-('558922894c453', 'cin<<a;', '558922895ea26'),
-('558922894c453', 'cout>>a;', '558922895ea34'),
-('558922894c453', 'cout<a;', '558922895ea41'),
-('558922899ccaa', 'cout', '55892289aa7cf'),
-('558922899ccaa', 'cin', '55892289aa7df'),
-('558922899ccaa', 'print', '55892289aa7eb'),
-('558922899ccaa', 'printf', '55892289aa7f5'),
-('558923538f48d', '255.0.0.0', '558923539a46c'),
-('558923538f48d', '255.255.255.0', '558923539a480'),
-('558923538f48d', '255.255.0.0', '558923539a48b'),
-('558923538f48d', 'none of these', '558923539a495'),
-('55892353f05c4', '192.168.1.100', '5589235405192'),
-('55892353f05c4', '172.168.16.2', '55892354051a3'),
-('55892353f05c4', '10.0.0.0.1', '55892354051b4'),
-('55892353f05c4', '11.11.11.11', '55892354051be'),
-('558973f4389ac', 'containing root file-system required during bootup', '558973f462e44'),
-('558973f4389ac', ' Contains only scripts to be executed during bootup', '558973f462e56'),
-('558973f4389ac', ' Contains root-file system and drivers required to be preloaded during bootup', '558973f462e61'),
-('558973f4389ac', 'None of the above', '558973f462e6b'),
-('558973f4c46f2', 'Kernel', '558973f4d4abe'),
-('558973f4c46f2', 'Shell', '558973f4d4acf'),
-('558973f4c46f2', 'Commands', '558973f4d4ad9'),
-('558973f4c46f2', 'Script', '558973f4d4ae3'),
-('558973f51600d', 'Boot Loading', '558973f526f9d'),
-('558973f51600d', ' Boot Record', '558973f526fb9'),
-('558973f51600d', ' Boot Strapping', '558973f526fc5'),
-('558973f51600d', ' Booting', '558973f526fce'),
-('558973f55d269', ' Quick boot', '558973f57aef1'),
-('558973f55d269', 'Cold boot', '558973f57af07'),
-('558973f55d269', ' Hot boot', '558973f57af17'),
-('558973f55d269', ' Fast boot', '558973f57af27'),
-('558973f5abb1a', 'bash', '558973f5e7623'),
-('558973f5abb1a', ' Csh', '558973f5e7636'),
-('558973f5abb1a', ' ksh', '558973f5e7640'),
-('558973f5abb1a', ' sh', '558973f5e764a'),
-('5589751a63091', 'q', '5589751a81bd6'),
-('5589751a63091', 'wq', '5589751a81be8'),
-('5589751a63091', ' both (a) and (b)', '5589751a81bf4'),
-('5589751a63091', ' none of the mentioned', '5589751a81bfd'),
-('5589751ad32b8', ' moves screen down one page', '5589751adbdbd'),
-('5589751ad32b8', 'moves screen up one page', '5589751adbdce'),
-('5589751ad32b8', 'moves screen up one line', '5589751adbdd8'),
-('5589751ad32b8', ' moves screen down one line', '5589751adbde2'),
-('5589751b304ef', ' yy', '5589751b3b04d'),
-('5589751b304ef', 'yw', '5589751b3b05e'),
-('5589751b304ef', 'yc', '5589751b3b069'),
-('5589751b304ef', ' none of the mentioned', '5589751b3b073'),
-('5589751b749c9', 'X', '5589751b9a98c'),
-('5589751b749c9', 'x', '5589751b9a9a5'),
-('5589751b749c9', 'D', '5589751b9a9b7'),
-('5589751b749c9', 'd', '5589751b9a9c9'),
-('5589751bd02ec', 'autoindentation is not possible in vi editor', '5589751bdadaa'),
-('5bd1a29b0514c', 'left-root-right', '5bd1a29b1c417'),
-('5bd1a29b0514c', 'root-left-right', '5bd1a29b1c42c'),
-('5bd1a29b0514c', 'right-left-root', '5bd1a29b1c42d'),
-('5bd1a29b0514c', 'none of these', '5bd1a29b1c42e'),
-('5bd1a29b7d4b8', 'left-root-right', '5bd1a29b8ae62'),
-('5bd1a29b7d4b8', 'root-left-right', '5bd1a29b8ae6e'),
-('5bd1a29b7d4b8', 'right-left-root', '5bd1a29b8ae70'),
-('5bd1a29b7d4b8', 'none of these', '5bd1a29b8ae71');
+('5ddd606a6ffbe', 'Apotik', '5ddd606a70736'),
+('5ddd606a6ffbe', 'Memasukan', '5ddd606a7073b'),
+('5ddd606a6ffbe', 'Memasukkan', '5ddd606a7073c'),
+('5ddd606a6ffbe', 'Ubat', '5ddd606a7073d'),
+('5ddd606a73dda', 'Cara menyimpan brokoli', '5ddd606a74475'),
+('5ddd606a73dda', 'Fungsi garam dan penirisan brokoli', '5ddd606a7447a'),
+('5ddd606a73dda', 'Cara mempertahankan warna brokoli', '5ddd606a7447b'),
+('5ddd606a73dda', 'Daya tahan brokoli', '5ddd606a7447c'),
+('5ddd63a65fc6f', 'give', '5ddd63a6603ce'),
+('5ddd63a65fc6f', 'giving', '5ddd63a6603d3'),
+('5ddd63a65fc6f', 'given', '5ddd63a6603d4'),
+('5ddd63a65fc6f', 'gave', '5ddd63a6603d5'),
+('5ddd63a662fb0', 'That', '5ddd63a663717'),
+('5ddd63a662fb0', 'Whom', '5ddd63a66371e'),
+('5ddd63a662fb0', 'Who', '5ddd63a66371f'),
+('5ddd63a662fb0', 'Whose', '5ddd63a663721'),
+('5ddd650773e47', 'Pisau Belati', '5ddd6507745ad'),
+('5ddd650773e47', 'Bambu Runcing', '5ddd6507745ba'),
+('5ddd650773e47', 'Palu dan Arit', '5ddd6507745be'),
+('5ddd650773e47', 'Markas musuh Power Ranger', '5ddd6507745c1'),
+('5ddd6507773f7', '23 Maret 1946', '5ddd650777a7c'),
+('5ddd6507773f7', '24 Maret 1946', '5ddd650777a7f'),
+('5ddd6507773f7', '25 Maret 1946', '5ddd650777a7f'),
+('5ddd6507773f7', '26 Maret 1946', '5ddd650777a81'),
+('5ddd66c058f87', 'Awareness of the contemporary fole of science', '5ddd66c05965e'),
+('5ddd66c058f87', 'A compassionate citizenry', '5ddd66c059662'),
+('5ddd66c058f87', 'Value judgements for a changing world', '5ddd66c059664'),
+('5ddd66c058f87', 'Readiness for effective economic life', '5ddd66c059665'),
+('5ddd66c05b4db', 'Marxis', '5ddd66c05c43d'),
+('5ddd66c05b4db', 'Pluralis', '5ddd66c05c442'),
+('5ddd66c05b4db', 'Organis', '5ddd66c05c443'),
+('5ddd66c05b4db', 'Fasis', '5ddd66c05c445'),
+('5ddd68c4a9ac3', '-2/(x - 1)^2', '5ddd68c4aa291'),
+('5ddd68c4a9ac3', '-x/(x+1)', '5ddd68c4aa298'),
+('5ddd68c4a9ac3', '2x/(x-1)^2', '5ddd68c4aa29a'),
+('5ddd68c4a9ac3', '-2x/(x-1)', '5ddd68c4aa29b'),
+('5ddd68c4adf85', '2(p+2q) ', '5ddd68c4ae7ef'),
+('5ddd68c4adf85', '2(2p+q) ', '5ddd68c4ae7f5'),
+('5ddd68c4adf85', '2p+q ', '5ddd68c4ae7f6'),
+('5ddd68c4adf85', 'p+2q', '5ddd68c4ae7f8'),
+('5ddd6a196b714', 'Shohih', '5ddd6a196bf21'),
+('5ddd6a196b714', 'Hasan', '5ddd6a196bf25'),
+('5ddd6a196b714', 'Dhoif', '5ddd6a196bf27'),
+('5ddd6a196b714', 'Mutawattir', '5ddd6a196bf28'),
+('5ddd6a196f4c9', 'Sanad Hadist', '5ddd6a197051a'),
+('5ddd6a196f4c9', 'Rawi Hadist', '5ddd6a1970527'),
+('5ddd6a196f4c9', 'Matan Hadist', '5ddd6a1970528'),
+('5ddd6a196f4c9', 'Tafsir Hadist', '5ddd6a1970529');
 
 -- --------------------------------------------------------
 
@@ -226,26 +222,18 @@ CREATE TABLE `questions` (
 --
 
 INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
-('558920ff906b8', '55892169bf6a7', 'what is command for changing user information??', 4, 1),
-('558920ff906b8', '5589216a3646e', 'what is permission for view only for other??', 4, 2),
-('558921841f1ec', '558922117fcef', 'what is command for print in php??', 4, 1),
-('558921841f1ec', '55892211e44d5', 'which is a variable of php??', 4, 2),
-('5589222f16b93', '558922894c453', 'what is correct statement in c++??', 4, 1),
-('5589222f16b93', '558922899ccaa', 'which command is use for print the output in c++?', 4, 2),
-('558922ec03021', '558923538f48d', 'what is correct mask for A class IP???', 4, 1),
-('558922ec03021', '55892353f05c4', 'which is not a private IP??', 4, 2),
-('55897338a6659', '558973f4389ac', 'On Linux, initrd is a file', 4, 1),
-('55897338a6659', '558973f4c46f2', 'Which is loaded into memory when system is booted?', 4, 2),
-('55897338a6659', '558973f51600d', ' The process of starting up a computer is known as', 4, 3),
-('55897338a6659', '558973f55d269', ' Bootstrapping is also known as', 4, 4),
-('55897338a6659', '558973f5abb1a', 'The shell used for Single user mode shell is:', 4, 5),
-('5589741f9ed52', '5589751a63091', ' Which command is used to close the vi editor?', 4, 1),
-('5589741f9ed52', '5589751ad32b8', ' In vi editor, the key combination CTRL+f', 4, 2),
-('5589741f9ed52', '5589751b304ef', ' Which vi editor command copies the current line of the file?', 4, 3),
-('5589741f9ed52', '5589751b749c9', ' Which command is used to delete the character before the cursor location in vi editor?', 4, 4),
-('5589741f9ed52', '5589751bd02ec', ' Which one of the following statement is true?', 4, 5),
-('5bd1a1a3c5e5b', '5bd1a29b0514c', 'What is inorder?', 4, 1),
-('5bd1a1a3c5e5b', '5bd1a29b7d4b8', 'what is pre order?', 4, 2);
+('5ddd5e51a7a39', '5ddd606a6ffbe', 'Menurut KBBI, kata baku yang benar adalah...', 4, 1),
+('5ddd5e51a7a39', '5ddd606a73dda', 'Bacalah paragraf berikut dengan saksama!\r\n\r\nSebaiknya, brokoli dimasak atau direbus dengan air mendidih dalam kondisi setengah matang. Tambahkan garam untuk membunuh kuman, lalu tiriskan sebentar! Siram dengan air dingin! Fungsi air dingin untuk mempertahankan warna dan memperlambat proses pemasakan brokoli. Dalam kondisi seperti ini, brokoli dapat disimpan 2—3 hari dalam lemari pendingin dan aman dikonsumsi. Ide pokok paragraf tersebut adalah....', 4, 2),
+('5ddd6320142aa', '5ddd63a65fc6f', 'Complete this senteces ! The poor kids thanked us for ... him money and food.', 4, 1),
+('5ddd6320142aa', '5ddd63a662fb0', 'The doctor carefully examined patients ... heads had injured in an accident.', 4, 2),
+('5ddd642381974', '5ddd650773e47', 'Monumen Perjuangan Rakyat Jawa Barat memiliki arsitektur seperti?', 4, 1),
+('5ddd642381974', '5ddd6507773f7', 'Peristiwa Bandung Lautan Api terjadi pada tanggal?', 4, 2),
+('5ddd6545d57ac', '5ddd66c058f87', 'Salah satu tujuan PKN adalah menumbuhkan perasaan belas kasihan terhadap rakyat atau menumbuhkan pandangan yang bisa menghargai manusia sebagai manusia, yang disebut dengan…', 4, 1),
+('5ddd6545d57ac', '5ddd66c05b4db', 'Negara bukan merupakan alat dari masyarakatnya. Negara merupakan alat dari dirinya sendiri. Hal tersebut merupakan pendapat teori...', 4, 2),
+('5ddd66f1d08b8', '5ddd68c4a9ac3', 'Tentukan fungsi turunan pertama dari f(x) = x + 1 / x - 1 ', 4, 1),
+('5ddd66f1d08b8', '5ddd68c4adf85', 'Diketahui 5log2=p dan 5log3=q. Nilai dari 5log144=⋯⋅', 4, 2),
+('5ddd694bca4d7', '5ddd6a196b714', 'Kebersihan sebagian dari Iman adalah termasuk ke dalam Hadist?', 4, 1),
+('5ddd694bca4d7', '5ddd6a196f4c9', 'Makna isi hadist disebut?', 4, 2);
 
 -- --------------------------------------------------------
 
@@ -262,7 +250,7 @@ CREATE TABLE `quiz` (
   `time` bigint(20) NOT NULL,
   `intro` text NOT NULL,
   `tag` varchar(100) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `email` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -271,13 +259,12 @@ CREATE TABLE `quiz` (
 --
 
 INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `tag`, `date`, `email`) VALUES
-('558920ff906b8', 'Linux : File Managment', 2, 1, 2, 5, '', 'linux', '2018-10-20 14:47:56', 'teacher2@gmail.com'),
-('558921841f1ec', 'Php Coding', 2, 1, 2, 5, '', 'PHP', '2018-10-20 14:47:04', 'teacher1@gmail.com'),
-('5589222f16b93', 'C++ Coding', 2, 1, 2, 5, '', 'c++', '2018-10-20 14:47:04', 'teacher1@gmail.com'),
-('558922ec03021', 'Networking', 2, 1, 2, 5, '', 'networking', '2018-10-20 14:47:04', 'teacher1@gmail.com'),
-('55897338a6659', 'Linux:startup', 2, 1, 5, 10, '', 'linux', '2018-10-20 14:47:56', 'teacher2@gmail.com'),
-('5589741f9ed52', 'Linux :vi Editor', 2, 1, 5, 10, '', 'linux', '2018-10-20 14:47:56', 'teacher2@gmail.com'),
-('5bd1a1a3c5e5b', 'Data Structure', 1, 0, 2, 1, 'ds exam', 'ds', '2018-10-25 10:57:39', 'teacher2@gmail.com');
+('5ddd5e51a7a39', '#bahasa Indonesia', 2, 1, 2, 5, 'pertanyaan ujian Bahasa Indonesia', '#bahasaindonesia', '2019-11-26 17:18:09', 'indonesia@gmail.com'),
+('5ddd6320142aa', '#bahasa Inggris', 2, 1, 2, 5, 'pertanyaan ujian Bahasa Inggris', '#bahasainggris', '2019-11-26 17:38:40', 'inggris@gmail.com'),
+('5ddd642381974', '#sejarah Indonesia', 2, 1, 2, 5, 'pertanyaan ujian Sejarah Indonesia', '#sejarah', '2019-11-26 17:42:59', 'sejarah@gmail.com'),
+('5ddd6545d57ac', '#pendidikan Pancasila Dan Kewarganegaraan', 2, 1, 2, 5, 'pertanyaan ujian Pendidikan dan Kewarganegaraan', '#pancasila', '2019-11-26 17:47:49', 'pancasila@gmail.com'),
+('5ddd66f1d08b8', '#matematika', 2, 1, 2, 10, 'pertanyaan ujian Matematika', '#matematika', '2019-11-26 17:54:57', 'matematika@gmail.com'),
+('5ddd694bca4d7', '#agama', 2, 1, 2, 5, 'pertanyaan ujian Agama', '#agama', '2019-11-26 18:04:59', 'agama@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -288,8 +275,24 @@ INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `
 CREATE TABLE `rank` (
   `email` varchar(50) NOT NULL,
   `score` int(11) NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  `time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `rank`
+--
+
+INSERT INTO `rank` (`email`, `score`, `time`) VALUES
+('fauziah@gmail.com', 1, '2019-11-27 01:19:00'),
+('ridwan@gmail.com', 4, '2019-11-27 01:19:43'),
+('asep@gmail.com', 4, '2019-11-27 01:20:48'),
+('ragil@gmail.com', -2, '2019-11-27 01:21:12'),
+('ahmad@gmail.com', 1, '2019-11-27 01:22:20'),
+('monica@gmail.com', 4, '2019-11-27 01:22:45'),
+('septian@gmail.com', -2, '2019-11-27 01:23:07'),
+('valent@gmail.com', -2, '2019-11-27 01:23:27'),
+('andhini@gmail.com', 8, '2019-11-27 01:24:17'),
+('anisa@gmail.com', -1, '2019-11-27 01:25:34');
 
 -- --------------------------------------------------------
 
@@ -307,6 +310,22 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
+('Ahmad', 'M', 'SMA 8 Bandung', 'ahmad@gmail.com', 87647478888, '61243c7b9a4022cb3f8dc3106767ed12'),
+('Andhini', 'F', 'SMA 4 Bandung', 'andhini@gmail.com', 87262525278, '1369615388917d5c0138c81dbd33fe0b'),
+('Anisa', 'F', 'SMA 13 Bandung', 'anisa@gmail.com', 82372362992, '40cc8f68f52757aff1ad39a006bfbf11'),
+('Asep', 'M', 'SMA N 1 Bandung', 'asep@gmail.com', 88132328811, '7b4449987d9a0e390f88e8fa37a12b83'),
+('Fauziah', 'F', 'SMA 24 Bandung', 'fauziah@gmail.com', 83658736883, 'a8e404443f4edea9337a04fa3bf192b1'),
+('Monica', 'F', 'SMA 7 Bandung', 'monica@gmail.com', 89237813222, 'ff0d813dd5d2f64dd372c6c4b6aed086'),
+('Ragil', 'M', 'SMA 7 Bandung', 'ragil@gmail.com', 86352769993, '67153c4ffb77b9d03276cad142a84e79'),
+('Ridwan', 'M', 'SMAN 2 Bandung', 'ridwan@gmail.com', 8712737123, 'd584c96e6c1ba3ca448426f66e552e8e'),
+('Septian', 'M', 'SMA 3 Bandung', 'septian@gmail.com', 82763713888, '5b3bb3e5458e02aa356f2fc671ae08d9'),
+('Valent', 'F', 'SMA 10 Bandung', 'valent@gmail.com', 89725522626, '441b680560ed6c0c5e49ff4c554a63e7');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -317,10 +336,26 @@ ALTER TABLE `admin`
   ADD PRIMARY KEY (`email`);
 
 --
+-- Indexes for table `audios`
+--
+ALTER TABLE `audios`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `audios`
+--
+ALTER TABLE `audios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
